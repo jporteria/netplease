a9581bb20cb833d315cb75ae7c5cf4e918b54397
wagako3 <cjporteria@gmail.com>
2024-01-14 11:26:21 +0800

    first commit-without error

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,5 +1,5 @@
 import 'react-slideshow-image/dist/styles.css'
-import React from 'react'git 
+
 //import { Fade, Zoom, Slide } from 'react-slideshow-image'
 
 export default function Preview(props){
@@ -8,7 +8,7 @@ export default function Preview(props){
     return(
             <div className="preview--box">
 
-                    <img width="100%" height="100%" src={props ? `https://www.themoviedb.org/t/p/original${props.movieList[2].backdrop_path}` : `https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg`} />
+                    <img width="100%" height="100%" src={props ? `https://www.themoviedb.org/t/p/original${props.movieList.backdrop_path}` : `https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg`} />
 
             </div>
         
