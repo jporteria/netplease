aa312c8be67163544889c3bb32e3d84d647f2533
jporteria <cjporteria@gmail.com>
2024-09-27 14:15:21 +0800

    debug

--- frontend/src/searched.tsx
+++ frontend/src/searched.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, Key } from "react";
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 
 export default function Searched(props: { focused: any; searchedMovie: any[]; }) {
 
