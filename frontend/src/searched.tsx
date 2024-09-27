7ee9863f67f8f8060d7b2a723198189b4f00cb3b
jporteria <cjporteria@gmail.com>
2024-09-27 15:11:09 +0800

    debug

--- frontend/src/searched.tsx
+++ frontend/src/searched.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, Key } from "react";
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export default function Searched(props: { focused: any; searchedMovie: any[]; }) {
 
@@ -7,8 +7,6 @@ export default function Searched(props: { focused: any; searchedMovie: any[]; })
     sessionStorage.setItem('movie', JSON.stringify(value))
     if(location.href == '/movieDetails'){
       location.reload()
-    }else{
-      location.href = '/movieDetails'
     }
   }
   // console.log(sessionStorage.getItem('movie'))
@@ -20,14 +18,14 @@ export default function Searched(props: { focused: any; searchedMovie: any[]; })
           original_title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }, 
           id: Key | null | undefined
         ) =>(
-          // <Link to="/movieDetails">
+          <Link to="/movieDetails">
             <div className="search--result" key={id} onClick={()=>movieClicked(movie)}>
                   <div className="movie--image">
                       <img className="movie--poster" width="30px" height="50px" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
                   </div>
                   <p className="search--movie--name">{movie.original_title}</p>
               </div>
-          // </Link>
+          </Link>
         ))}
     </div>
   )
