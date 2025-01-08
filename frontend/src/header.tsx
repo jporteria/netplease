b5d6a4ec4ba8cff56f4bab7d8791fed2da25e2b5
jporteria <cjporteria@gmail.com>
2025-01-08 13:20:25 +0800

    added responsive design

--- frontend/src/header.tsx
+++ frontend/src/header.tsx
@@ -5,143 +5,162 @@ import AuthForm from "./user/authForm";
 import { MovieContext } from "./App";
 //import { MovieContext } from "./App";
 
-export default function Header(){
+export default function Header() {
+  function GoToHomePage() {
+    window.location.href = "/";
+  }
 
-    function GoToHomePage(){
-            window.location.href = '/';   
-        }
+  const { setAuth, authToken, setAuthToken } = useContext(MovieContext);
 
-        const { setAuth, authToken, setAuthToken } = useContext(MovieContext)
+  const [searchedMovie, setSearchedMovie] = useState([]);
+  const [searchText, setSearchText] = useState({ title: "" });
 
+  const getSearchedMovie = () => {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${searchText.title}&api_key=649f645abf4721a3027659369cc67c24`
+    )
+      .then((response) => response.json())
+      .then((data) => setSearchedMovie(data.results));
+  };
 
-        const [searchedMovie, setSearchedMovie] = useState([])
-        const [searchText, setSearchText] = useState({title:""})
-        
-        const getSearchedMovie = () => {
-            fetch(`https://api.themoviedb.org/3/search/movie?query=${searchText.title}&api_key=649f645abf4721a3027659369cc67c24`)
-            .then(response => response.json())
-            .then(data => setSearchedMovie(data.results))
-        }
+  function handleChange(event: { target: { value: any } }) {
+    const { value } = event.target;
+    setSearchText((prevSearch) => ({
+      ...prevSearch,
+      title: value,
+    }));
+    getSearchedMovie();
+  }
 
+  const [focused, setFocused] = useState(false);
+  const onFocus = () => setFocused(true);
+  const onBlur = () => setFocused(false);
 
-            function handleChange(event: { target: { value: any; }; }){
-            const { value } = event.target
-            setSearchText(prevSearch => ({
-                ...prevSearch,
-                title: value
-            }))
-                getSearchedMovie()
-            }
+  // const [auth, setAuth] = useState('')
+  // const [user, setUser] = useState({firstName: '', lastName: '', email: '', password: ''})
 
-        const [focused, setFocused] = useState(false)
-        const onFocus = () => setFocused(true)
-        const onBlur = () => setFocused(false)
+  function showSignUpForm() {
+    const auth = document.getElementById("showAuthForm");
+    if (auth) {
+      auth.className = "auth--form--show";
+    }
+    setAuth("Signup");
+  }
+  function showLoginForm() {
+    const auth = document.getElementById("showAuthForm");
+    if (auth) {
+      auth.className = "auth--form--show";
+    }
+    setAuth("Login");
+  }
 
-        // const [auth, setAuth] = useState('')
-        // const [user, setUser] = useState({firstName: '', lastName: '', email: '', password: ''})
+  const [userProfile, setUserProfile] = useState("user--profile");
+  // const authToken = sessionStorage.getItem('auth-token')
+  const firstname = sessionStorage.getItem("auth-firstname");
+  const lastname = sessionStorage.getItem("auth-lastname");
 
-        function showSignUpForm(){
-            const auth = document.getElementById('showAuthForm')
-            if(auth){
-                auth.className = "auth--form--show"
-            }
-            setAuth('Signup')
-        }
-        function showLoginForm(){
-            const auth = document.getElementById('showAuthForm')
-            if(auth){
-                auth.className = "auth--form--show"
-            }
-            setAuth('Login')
-        }
+  function showUserProfile() {
+    if (userProfile == "user--profile") {
+      setUserProfile("show--user--profile");
+    } else {
+      setUserProfile("user--profile");
+    }
+  }
 
-        const [userProfile, setUserProfile] = useState('user--profile')
-        // const authToken = sessionStorage.getItem('auth-token')
-        const firstname = sessionStorage.getItem('auth-firstname')
-        const lastname = sessionStorage.getItem('auth-lastname')
-        
-
-        function showUserProfile(){
-            if(userProfile == 'user--profile'){
-                setUserProfile('show--user--profile')
-            }else{
-                setUserProfile('user--profile')
-            }
-        }
-
-
-    return(
-        <div className="top">
-            {/* <div className='viewError'>
+  return (
+    <div className="top">
+      {/* <div className='viewError'>
                     <h1>For better experience, please use desktop view mode</h1>
             </div> */}
-        <div className="header">
-            <div className="site--name"
-                onClick={GoToHomePage}
-            >
-                {/* <img className="header--logo" src="../image/jc.jpg" /> */}
-                Netplease
-            </div>
-            <div className="header--movies">
-                <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/popularMovies">Popular Movies</NavLink>
-                <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/upcomingMovies">Upcoming Movies</NavLink>
-                <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/tvSeries">TV Series</NavLink>
-                <NavLink className={({ isActive }) => (isActive ? 'active' : 'inactive')} to="/topRatedTvSeries">Top Rated TV Series</NavLink>
-                <div className="header--search">
-                    <input 
-                        onBlur={onBlur}
-                        onFocus={onFocus}
-                        type="search"
-                        placeholder="Search"
-                        className="searchField"
-                        onChange={handleChange}
-                        value={searchText.title}
-                    />
-                    {/* <button 
+      <div className="header">
+        <div className="site--name" onClick={GoToHomePage}>
+          {/* <img className="header--logo" src="../image/jc.jpg" /> */}
+          Netplease
+        </div>
+        <div className="header--movies">
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/popularMovies"
+          >
+            Popular Movies
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/upcomingMovies"
+          >
+            Upcoming Movies
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/tvSeries"
+          >
+            TV Series
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? "active" : "inactive")}
+            to="/topRatedTvSeries"
+          >
+            Top Rated TV Series
+          </NavLink>
+          <div className="header--search">
+            <input
+              onBlur={onBlur}
+              onFocus={onFocus}
+              type="search"
+              placeholder="Search"
+              className="searchField"
+              onChange={handleChange}
+              value={searchText.title}
+            />
+            {/* <button 
                         className="searchButton" 
                         style={{backgroundImage:"url('../image/search.png')", 
                         backgroundSize: "cover"}}>  
                     </button> */}
-                </div>
-            </div>
-                {
-                    authToken 
-                    ? 
-                    <div className="header--user" onClick={showUserProfile}>
-                        <img className='user--icon' src="https://drive.google.com/thumbnail?id=1dgRZJeX4kNcdsv2xQIFnvppTdNJBh3jp&sz=w1000" alt="no user" />
-                    </div> 
-                    : 
-                    <div className="header--user">
-                        <div className='user--signUp' onClick={showSignUpForm}>
-                            Sign Up
-                        </div>
-                        <div className="user--login" onClick={showLoginForm}>
-                            Login
-                        </div>
-                    </div>
-                }
+          </div>
         </div>
-        <div className={userProfile}>
-            <div className="user--name">{`${firstname} ${lastname}`}</div>
-            <div className="user--options">
-                <div className="watchList">Watch List</div>
-                <div className="reviews">Reviews</div>
-                <div className="settings">Account Settings</div>
-                <div className="logout" onClick={() => {
-                                                        setUserProfile('user--profile'),
-                                                        sessionStorage.removeItem('auth-token'),
-                                                        sessionStorage.removeItem('auth-firstname'),
-                                                        sessionStorage.removeItem('auth-lastname'),
-                                                        setAuthToken('')
-                                                        }}>Logout</div>
+        {authToken ? (
+          <div className="header--user" onClick={showUserProfile}>
+            <img
+              className="user--icon"
+              src="https://drive.google.com/thumbnail?id=1dgRZJeX4kNcdsv2xQIFnvppTdNJBh3jp&sz=w1000"
+              alt="no user"
+            />
+          </div>
+        ) : (
+          <div className="header--user">
+            <div className="user--signUp" onClick={showSignUpForm}>
+              Sign Up
             </div>
+            <div className="user--login" onClick={showLoginForm}>
+              Login
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={userProfile}>
+        <div className="user--name">{`${firstname} ${lastname}`}</div>
+        <div className="user--options">
+          <div className="watchList">Watch List</div>
+          <div className="reviews">Reviews</div>
+          <div className="settings">Account Settings</div>
+          <div
+            className="logout"
+            onClick={() => {
+              setUserProfile("user--profile"),
+                sessionStorage.removeItem("auth-token"),
+                sessionStorage.removeItem("auth-firstname"),
+                sessionStorage.removeItem("auth-lastname"),
+                setAuthToken("");
+            }}
+          >
+            Logout
+          </div>
         </div>
-        <Searched 
-        searchedMovie={searchedMovie}
-        focused={focused}
-        />
-        <AuthForm />
-    <Outlet />
+      </div>
+      <Searched searchedMovie={searchedMovie} focused={focused} />
+      <AuthForm />
+      <Outlet />
     </div>
-    )
+  );
 }
--- frontend/src/preview.tsx
+++ frontend/src/preview.tsx
@@ -1,58 +1,76 @@
-import React from 'react'
+import React from "react";
 // import { Slide } from 'react-slideshow-image'
-import 'react-slideshow-image/dist/styles.css'
+import "react-slideshow-image/dist/styles.css";
 
-export default function Preview(){
-        const [trendingList, setTrendingList] = React.useState([])
+export default function Preview() {
+  const [trendingList, setTrendingList] = React.useState([]);
 
-        const getTrending = () => {
-                fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=649f645abf4721a3027659369cc67c24')
-                .then(response => response.json())
-                .then(data => setTrendingList(data.results))
-        }
-        React.useEffect(() => {
-                getTrending()
-                },[])
-        
-        // const slideImage = [{trendingList}]
-
-        function handleWindowScroll(arrow: HTMLElement): void {
-                window.addEventListener('scroll', () => {
-                  // Use the 'arrow' passed in as a parameter, no need to query it again
-                  if (window.scrollY > 20) {
-                    arrow.classList.add('arrow--hide');
-                    arrow.classList.remove('arrow--down');
-                  } else {
-                    arrow.classList.add('arrow--down');
-                    arrow.classList.remove('arrow--hide');
-                  }
-                });
-              }
-        const arrowDiv = document.getElementById('arrow') as HTMLElement;
-
-              // Call the function with the arrow element if it exists
-              if (arrowDiv) {
-                handleWindowScroll(arrowDiv);
-              }
-              
-
-    return(
-            <div>
-                {trendingList.slice(0,1).map((movie: {backdrop_path:any; original_title:any; id: any | null |undefined;}) =>(
-                        <div className="preview--box" key={movie.id}>
-                                <img className='preview--image' width="100%" height="100%" key={movie.id} src={`https://www.themoviedb.org/t/p/original${movie.backdrop_path}`} />
-                                {/* <h1 className="preview--title">{movie.original_title}</h1> */}
-                                <div className="preview--text">
-                                        <h1>Unlock the Magic of Cinema</h1>
-                                        <h2>Dive into Expert Reviews and Discover the Finest Films!</h2>
-                                </div>
-                                <div id="arrow" className="arrow arrow--down">
-                                        <img width="100%" height="100%" src="https://drive.google.com/thumbnail?id=1Wbr9InCz4paNVd2AP18UUIhrHAdVrmgi&sz=w1000" alt="" />
-                                </div>
-                        </div>
-                ))}
-            </div>
-        
-        
+  const getTrending = () => {
+    fetch(
+      "https://api.themoviedb.org/3/trending/movie/day?api_key=649f645abf4721a3027659369cc67c24"
     )
+      .then((response) => response.json())
+      .then((data) => setTrendingList(data.results));
+  };
+  React.useEffect(() => {
+    getTrending();
+  }, []);
+
+  // const slideImage = [{trendingList}]
+
+  function handleWindowScroll(arrow: HTMLElement): void {
+    window.addEventListener("scroll", () => {
+      // Use the 'arrow' passed in as a parameter, no need to query it again
+      if (window.scrollY > 20) {
+        arrow.classList.add("arrow--hide");
+        arrow.classList.remove("arrow--down");
+      } else {
+        arrow.classList.add("arrow--down");
+        arrow.classList.remove("arrow--hide");
+      }
+    });
+  }
+  const arrowDiv = document.getElementById("arrow") as HTMLElement;
+
+  // Call the function with the arrow element if it exists
+  if (arrowDiv) {
+    handleWindowScroll(arrowDiv);
+  }
+
+  return (
+    <div>
+      {trendingList
+        .slice(0, 1)
+        .map(
+          (movie: {
+            backdrop_path: any;
+            original_title: any;
+            id: any | null | undefined;
+          }) => (
+            <div className="preview--box" key={movie.id}>
+              <img
+                className="preview--image"
+                width="100%"
+                // height="100%"
+                key={movie.id}
+                src={`https://www.themoviedb.org/t/p/original${movie.backdrop_path}`}
+              />
+              {/* <h1 className="preview--title">{movie.original_title}</h1> */}
+              <div className="preview--text">
+                <h1>Unlock the Magic of Cinema</h1>
+                <h2>Dive into Expert Reviews and Discover the Finest Films!</h2>
+              </div>
+              <div id="arrow" className="arrow arrow--down">
+                <img
+                  width="100%"
+                  height="100%"
+                  src="https://drive.google.com/thumbnail?id=1Wbr9InCz4paNVd2AP18UUIhrHAdVrmgi&sz=w1000"
+                  alt=""
+                />
+              </div>
+            </div>
+          )
+        )}
+    </div>
+  );
 }
